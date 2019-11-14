b89e069febe56d64784ff048ad6aceb0b3201b24
dgathogo <edgetech26@gmail.com>
2019-11-15 00:08:05 +0100

    splash screen

--- app/src/main/java/us/ait/shoppinglist/SplashActivity.kt
+++ app/src/main/java/us/ait/shoppinglist/SplashActivity.kt
@@ -3,7 +3,9 @@ package us.ait.shoppinglist
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.acitivity_splash.*
 
 
 class SplashActivity : AppCompatActivity() {
@@ -11,11 +13,16 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.acitivity_splash)
+        var demoAnim = AnimationUtils.loadAnimation(
+            this@SplashActivity,
+            R.anim.splash_animation)
+        ivIcon.startAnimation(demoAnim)
         Handler().postDelayed({
             val intent = Intent(this, ScrollingActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }, 1500)
 
     }
 }
